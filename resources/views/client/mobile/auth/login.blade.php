3b0a54450da3de59a36dc3e8ed5a7fac11a83b65
m-rukhshona-a <j.rukhshona@gmail.com>
2021-05-17 15:55:28 +0500

    Login view for client completed

--- resources/views/client/mobile/auth/login.blade.php
+++ resources/views/client/mobile/auth/login.blade.php
@@ -6,6 +6,7 @@
 
 
 @section('content')
+
     <div class="jbg_grey01" style="position:relative; min-height:100%; padding-bottom:20px;">
 
         <!-- LOGO -->
@@ -18,24 +19,28 @@
         <!--// LOGO -->
 
         <!-- outermost_margin designation -->
+    <form action="{{route('client.mobile.login.attempt')}}" method="post">
+            {{ csrf_field() }}
         <div style="padding:30px 15px 0;">
             <!-- input -->
             <fieldset class="login_border">
                 <legend class="jm_tsss0 jcr_grey9" style="width:43px; padding:0 5px; height:15px;">ID</legend>
-                <input class="w_100 js_input00" name="" placeholder="" type="text" style="font-size:1.30rem;">
+                <input class="w_100 js_input00" name="name" placeholder="" type="text" style="font-size:1.30rem;">
             </fieldset>
             <fieldset class="mgn_t10 login_border">
                 <legend class="jm_tsss0 jcr_grey9" style="width:53px; padding:0 5px; height:15px;">password</legend>
-                <input class="w_100 js_input00" name="" placeholder="" type="password">
+                <input class="w_100 js_input00" name="password" placeholder="" type="password">
             </fieldset>
             <div style="height:30px;">
-                <div class="mgn_t10 jm_tsss1 jcr_ylw j_bold">
-                    ID or password is incorrect.
-                </div>
+                @if ($errors->any())
+                    <div class="mgn_t10 jm_tsss1 jcr_ylw j_bold">
+                        ID or password is incorrect.
+                    </div>
+                @endif
             </div>
             <!-- // input -->
             <!-- button -->
-            <div class="flx_c jbg_ylw jm_tss1 jcr_wht j_bold" style="padding:19px 0; border-radius:0px;" onclick="location.href='index.html'">
+            <div class="flx_c jbg_ylw jm_tss1 jcr_wht j_bold" style="padding:19px 0; border-radius:0px;" onclick="javascript:this.form.submit();">
                 login
             </div>
             <!--// button -->
@@ -77,6 +82,7 @@
             <!-- // SNS_Login -->
         </div>
         <!--// Outermost_margin designation -->
+    </form>
     </div>
 @endsection
 
