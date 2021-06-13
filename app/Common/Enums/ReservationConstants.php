b589bafa1fd60a144e1d235030f2e3cef0ef7d59
firdavsibodullaev <firdavs3253@gmail.com>
2021-06-13 12:33:29 +0500

    reservation status constants

--- app/Common/Enums/ReservationConstants.php
+++ app/Common/Enums/ReservationConstants.php
@@ -3,8 +3,26 @@
 
 namespace App\Common\Enums;
 
-
+/**
+ * Class ReservationConstants
+ * @package App\Common\Enums
+ */
 class ReservationConstants
 {
 
+    const PAYED = "payed";
+    const CANCELED = "canceled";
+    const RESERVED = "reserved";
+
+    /**
+     * @return array
+     */
+    public static function statuses(): array
+    {
+        return [
+            self::PAYED,
+            self::RESERVED,
+            self::CANCELED,
+        ];
+    }
 }
